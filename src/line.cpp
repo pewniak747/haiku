0f2a743d85927d673ea40bfe11ccaf8e3928a24d
Tomasz Pewiński <pewniak747@gmail.com>
2012-11-25 21:47:08 +0100

    substitute words in line templates

--- src/line.cpp
+++ src/line.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include "line.h"
 
 Line::Line(std::vector<Word*> words, LineTemplate *lineTemplate) {
@@ -6,5 +8,14 @@ Line::Line(std::vector<Word*> words, LineTemplate *lineTemplate) {
 }
 
 std::string Line::getJapanese() {
-  return this->lineTemplate->getJapaneseTemplate();
+  std::string resultString = this->lineTemplate->getJapaneseTemplate();
+  for(unsigned i = 0; i < this->words.size(); i++) {
+    std::string replacementString = this->words[i]->getJapanese();
+    std::ostringstream replacedString;
+    replacedString << "$" << (i+1);
+    int findPosition = resultString.find(replacedString.str());
+    if(findPosition >= 0)
+      resultString.replace(findPosition, 2, replacementString);
+  }
+  return resultString;
 }
